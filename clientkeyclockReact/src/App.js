ff55c91d701303cb98dc9207efb39aef3b3e883c
unknown <elton.silva.ext@vr.com.br>
2022-01-12 17:09:25 -0300

    first commit client

---
+++ clientkeyclockReact/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import WelcomePage from "./pages/Homepage";
+import SecuredPage from "./pages/Securedpage";
+import UsersList from "./pages/user/UsersList";
+import keycloak from "./Keycloak"
+import CreateUser from "./pages/user/CreateUser";
+import UpdateUser from "./pages/user/UpdateUser";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import PrivateRoute from "./helpers/PrivateRoute";
+
+function App() {
+ return (
+   <div>
+     <ReactKeycloakProvider authClient={keycloak}>
+     <Nav />
+     <BrowserRouter>
+       <Routes>
+         <Route exact path="/" element={<WelcomePage />} />
+         <Route exact path="/users" element={<UsersList />} />
+         <Route exact path='/create' element={<CreateUser />} />
+         <Route exact path='/update/:id' element={<UpdateUser />}/>
+         <Route
+             path="/secured"
+             element={
+               <PrivateRoute>
+                 <SecuredPage />
+               </PrivateRoute>
+             }
+           />
+       </Routes>
+     </BrowserRouter>
+     </ReactKeycloakProvider>
+   </div>
+ );
+}
+
+export default App;
