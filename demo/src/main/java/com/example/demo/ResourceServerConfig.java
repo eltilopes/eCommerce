bacea86433376578bb2b47c28d24e45092e011e5
unknown <elton.silva.ext@vr.com.br>
2022-01-12 17:00:36 -0300

    first commit

---
+++ demo/src/main/java/com/example/demo/ResourceServerConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    private final KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter = new KeycloakJwtAuthenticationConverter();
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authz -> authz.antMatchers("/security/**").authenticated())
+                .oauth2ResourceServer()
+                .jwt().jwtAuthenticationConverter(keycloakJwtAuthenticationConverter);
+    }
+}
